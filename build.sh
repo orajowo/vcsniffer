049029fffa13cc188d54ea2ec49cca03d582046e
orajowo <miunprime@outlook.com>
2025-07-15 01:13:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ echo "Installing Playwright browsers using specific python version..."
 
 # [FIX] Gunakan python3.11 secara eksplisit untuk menjalankan modul playwright.
 # Pastikan versi ini (3.11) sama dengan yang Anda atur di dasbor Vercel.
-python3.11 -m playwright install --with-deps chromium
+playwright install --with-deps chromium
 
 echo "Build script finished."
